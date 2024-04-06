d46f96bdacfc5ac5a827ec00bb4d5510fe68e969
JonazScarpetta <tecnimundoseguridad@gmail.com>
2024-04-06 13:35:28 -0500

    cuarto prueba carpetta

--- JS/main.js
+++
@@ -1,8 +0,0 @@
-function mostrarformulario() {
-  const nombre = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const comentarios = document.getElementById("coments").value;
-
-  const anuncio = `gracia por darnos tu datos: ${nombre}. \n email = ${email} \n ${comentarios} `;
-  alert(anuncio);
-}
